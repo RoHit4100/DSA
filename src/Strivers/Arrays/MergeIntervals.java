18e1f459fea0c63d472ae117dd21098e35745b3b
RoHit4100 <rohitudamale777@gmail.com>
2024-08-21 17:59:20 +0530

    merging two intervals problem is solved

--- src/Strivers/Arrays/MergeIntervals.java
+++ src/Strivers/Arrays/MergeIntervals.java
@@ -32,4 +32,34 @@ public class MergeIntervals {
         // Convert the list of intervals to a 2D array
         return mergedList.toArray(new int[mergedList.size()][]);
     }
+
+
+
+    // coding ninja
+    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+        int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            // if the current interval does not
+            // lie in the last interval:
+            if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1).get(1)) {
+                ans.add(Arrays.asList(arr[i][0], arr[i][1]));
+            }
+            // if the current interval
+            // lies in the last interval:
+            else {
+                ans.get(ans.size() - 1).set(1,
+                        Math.max(ans.get(ans.size() - 1).get(1), arr[i][1]));
+            }
+        }
+        return ans;
+    }
 }
