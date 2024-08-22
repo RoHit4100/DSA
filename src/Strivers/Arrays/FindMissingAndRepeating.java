09b46a8f7b607a272cb20296768551b77bc5249f
RoHit4100 <rohitudamale777@gmail.com>
2024-08-22 15:01:23 +0530

    2 more questions solved

---
+++ src/Strivers/Arrays/FindMissingAndRepeating.java
@@ -0,0 +1,102 @@
+package Strivers.Arrays;
+
+import java.util.Arrays;
+
+public class FindMissingAndRepeating {
+    // now using the hashing
+    static int[] findMissingAndRepeating(int[] arr){
+        // first create a hash array with size + 1 length
+        int[] hash = new int[arr.length + 1];
+
+        // hash the given array
+        for (int j : arr) {
+            hash[j]++;
+        }
+        int repeating = -1;
+        int missing = -1;
+
+        // now find the index into the hash which has value 2 and 0
+        for(int i = 1; i < hash.length;i++){
+            if(hash[i] == 0){
+                missing = i;
+            }else if(hash[i] == 2){
+                repeating = i;
+            }
+
+            if(repeating == -1 && missing == -1){
+                break;
+            }
+        }
+        return new int[] {repeating, missing};
+    }
+    public static void main(String[] args) {
+        int[] arr = {8 ,4 ,1 ,6 ,7 ,2 ,5 ,8 };
+        System.out.println(Arrays.toString(findMissingRepeatingNumbers(arr)));
+        System.out.println(Arrays.toString(findMissingAndRepeating(arr)));
+    }
+
+    // using cyclic sort
+    public static int[] findMissingRepeatingNumbers(int []arr) {
+        // Write your code here
+        // in this problem I will use the Cyclic sort algorithm
+
+        int[] ans = new int[2];
+        // first sort the array
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }else{
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        // now check for the missing and repeating
+        for(i = 0; i < arr.length; i++){
+            if(arr[i] != i + 1){
+                ans[0] = arr[i]; // repeating
+                ans[1] = i + 1; // missing
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
+
+
+
+    // find missing in 2D matrix with hashing
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int size = n * n;
+        int[] count = new int[size + 1];  // To count occurrences of each number
+        int a = -1, b = -1;
+
+        // Count the frequency of each number in the grid
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                count[grid[i][j]]++;
+            }
+        }
+
+        // Find the repeating (a) and missing (b) numbers
+        for (int i = 1; i <= size; i++) {
+            if (count[i] == 0) {
+                b = i;  // Missing number
+            } else if (count[i] == 2) {
+                a = i;  // Repeating number
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
---
+++ src/Strivers/Arrays/MergeSortedArrays.java
@@ -0,0 +1,125 @@
+package Strivers.Arrays;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    // type 2
+    public static void mergeTwoSortedArraysWithoutExtraSpace(long []arr1, long []arr2){
+        // Write your code here.
+        int n = arr1.length;
+        int m = arr2.length;
+        // Declare 2 pointers:
+        int left = n - 1;
+        int right = 0;
+
+        // Swap the elements until arr1[left] is
+        // smaller than arr2[right]:
+        while (left >= 0 && right < m) {
+            if (arr1[left] > arr2[right]) {
+                long temp = arr1[left];
+                arr1[left] = arr2[right];
+                arr2[right] = temp;
+                left--;
+                right++;
+            } else {
+                break;
+            }
+        }
+
+        // Sort arr1[] and arr2[] individually:
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+    }
+
+
+    // type 1
+    // optimal approach
+    public static void mergeOptimal(int[] nums1, int m, int[] nums2, int n) {
+        // here I will use the two pointer method
+        int i = m - 1; // which will be the last index of the nums1
+        int j = n - 1; // this will be the last index of the nums2
+        int k = m + n - 1; // which will the last index of the nums1;
+
+        // now traverse through array until any of the i or j less than 0
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[k] = nums1[i]; // as larger element will be at kth location
+                i--;
+            }else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        mergeOptimal(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    // brute force
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        // let's go for the first brute force approach
+
+        if(m + n == 1){
+            return;
+        }
+
+        int[] ans = new int[m + n];
+
+        int i = 0; // for nums1
+        int j = 0; // for nums2
+        int k = 0; // for ans
+
+        while(i < m && j < n){
+            if(nums1[i] < nums2[j]){
+                ans[k] = nums1[i];
+                i++;
+            }else{
+                ans[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        // in the end there will be some elements left
+        while(i < m){
+            ans[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while(j < n){
+            ans[k] = nums2[j];
+            j++;
+            k++;
+        }
+        System.out.println(Arrays.toString(ans));
+        // now add all the elements into nums1
+        k = 0;
+        i = 0;
+        while(i < ans.length){
+            nums1[i] = ans[k];
+            i++;
+            k++;
+        }
+    }
+
+
+    // better approach
+    public void mergeBetter(int[] nums1, int m, int[] nums2, int n) {
+        int i = m; // from this index I will add the elements of nums2
+        int j = 0;
+
+        while(i < m + n && j < n){
+            nums1[i] = nums2[j];
+            i++;
+            j++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+
